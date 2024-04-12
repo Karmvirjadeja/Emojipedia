b17319f90fa52ce2893fcf1ee0c16f0d6285841f
Knight <112848592+Karmvirjadeja@users.noreply.github.com>
2024-04-12 09:15:33 +0530

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,44 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id:4 ,
+    emoji: "🌟",
+    name: "Glowing Star",
+    meaning: "A classic, five-point gold star. Commonly used for various metaphorical senses of star (e.g., for fame, success, excellence) and literal stars in the sky at night."
+  },
+  {
+    id: 5,
+    emoji: "🍕",
+    name: "Pizza",
+    meaning: "A slice of pepperoni pizza, topped with black olives on Google. WhatsApp includes green peppers, Samsung white onion. May be used for various content concerning pizza, as love for it or cravings."
+  },
+  {
+    id: 6,
+    emoji: "🎉",
+    name: "Party Popper",
+    meaning: "A colorful party popper, used for party or other celebration-like occasions (e.g., birthday, New Year's Eve, success)."
+  }
+];
+
+
+export default emojipedia;
